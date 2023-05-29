a582eeaed1e31c7761c2a2f88d13929151abaffa
TJ Klug <tj@uniquity.ai>
2023-05-29 09:08:38 -0500

    push new changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,8 +2,7 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "amplifypoc": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
     },
     "apiOpenAI": {
       "ApiId": "string",
