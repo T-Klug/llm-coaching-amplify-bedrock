06cea218f786a7fdb16498297ead099a2a81d161
TJ Klug <tj@uniquity.ai>
2023-05-31 09:02:43 -0500

    Fix error handling

--- amplify/backend/function/createOpenAiChat/src/index.js
+++ amplify/backend/function/createOpenAiChat/src/index.js
@@ -185,16 +185,11 @@ export const handler = async (event) => {
     chatModel = updateChatModel(chatModel, res.data.choices[0].message);
   } catch (error) {
     if (error.response?.status) {
-      console.error(error.response.status, error.message);
-      error.response.data.on("data", (data) => {
-        const message = data.toString();
-        try {
-          const parsed = JSON.parse(message);
-          console.error("An error occurred during OpenAI request: ", parsed);
-        } catch (error) {
-          console.error("An error occurred during OpenAI request: ", message);
-        }
-      });
+      console.error(
+        "An error occurred during OpenAI request: ",
+        error.response.status,
+        error.message
+      );
     } else {
       console.error("An error occurred during OpenAI request", error);
     }
