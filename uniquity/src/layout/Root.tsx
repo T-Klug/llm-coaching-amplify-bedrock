ca391e1e5002a5bfa92917513fe6ad1a049af9c5
TJ Klug <tj@uniquity.ai>
2023-06-11 09:26:46 -0500

    Add update endpoint to register groups and the address of the user for in app messaging.

--- uniquity/src/layout/Root.tsx
+++ uniquity/src/layout/Root.tsx
@@ -1,8 +1,25 @@
 import { Outlet } from 'react-router-dom';
 
 import { Container } from '@mui/material';
+import { useEffect } from 'react';
+import { Analytics } from 'aws-amplify';
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 export default function Root() {
+  const { user } = useAuthenticator();
+  useEffect(() => {
+    Analytics.updateEndpoint({
+      address: user.attributes?.email, // The unique identifier for the recipient. For example, an address could be a device token, email address, or mobile phone number.
+      attributes: {
+        group: user.getSignInUserSession()?.getAccessToken().payload[
+          'cognito:groups'
+        ],
+      },
+      optOut: 'NONE',
+      // Customized userId
+      userId: user.attributes?.email,
+    });
+  }, [user]);
   return (
     <Container>
       <Outlet />
