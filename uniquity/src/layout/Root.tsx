55d3a6ba68da83920c3033627eefe82e66ede4e0
TJ Klug <tj@uniquity.ai>
2023-06-27 08:50:41 -0500

    add pendo

--- uniquity/src/layout/Root.tsx
+++ uniquity/src/layout/Root.tsx
@@ -30,6 +30,38 @@ export default function Root() {
         },
       });
       await Analytics.record({ name: 'Root-Loaded' });
+      // This function creates anonymous visitor IDs in Pendo unless you change the visitor id field to use your app's values
+      // This function uses the placeholder 'ACCOUNT-UNIQUE-ID' value for account ID unless you change the account id field to use your app's values
+      // Call this function in your authentication promise handler or callback when your visitor and account id values are available
+      // Please use Strings, Numbers, or Bools for value types.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      pendo.initialize({
+        visitor: {
+          id: user.username, // Required if user is logged in, default creates anonymous ID
+          email: user.attributes?.email, // Recommended if using Pendo Feedback, or NPS Email
+          // full_name:    // Recommended if using Pendo Feedback
+          role: user.getSignInUserSession()?.getAccessToken().payload[
+            'cognito:groups'
+          ], // Optional
+
+          // You can add any additional visitor level key-values here,
+          // as long as it's not one of the above reserved names.
+        },
+
+        account: {
+          id: window.location.hostname, // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
+          // name:         // Optional
+          // is_paying:    // Recommended if using Pendo Feedback
+          // monthly_value:// Recommended if using Pendo Feedback
+          // planLevel:    // Optional
+          // planPrice:    // Optional
+          // creationDate: // Optional
+
+          // You can add any additional account level key-values here,
+          // as long as it's not one of the above reserved names.
+        },
+      });
     };
     setupMessaging();
     // eslint-disable-next-line react-hooks/exhaustive-deps
