d448b81e7f72b65ddf8d2b4ef61781984695609d
TJ Klug <tj@uniquity.ai>
2023-08-23 12:32:24 -0500

    Add initial stub of coaching amount

--- uniquity/src/pages/Landing.tsx
+++ uniquity/src/pages/Landing.tsx
@@ -1,3 +1,81 @@
+import Typography from '@mui/material/Typography';
+import Rating, { IconContainerProps } from '@mui/material/Rating';
+import {
+  Looks3Outlined,
+  Looks4Outlined,
+  Looks5Outlined,
+  LooksOneOutlined,
+  LooksTwoOutlined,
+} from '@mui/icons-material';
+import { styled } from '@mui/material';
+import Divider from '@mui/material/Divider';
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#277A37',
+  },
+});
+
+const customIcons: {
+  [index: string]: {
+    icon: React.ReactElement;
+    label: string;
+  };
+} = {
+  1: {
+    icon: <LooksOneOutlined fontSize="inherit" />,
+    label: 'One',
+  },
+  2: {
+    icon: <LooksTwoOutlined fontSize="inherit" />,
+    label: 'Two',
+  },
+  3: {
+    icon: <Looks3Outlined fontSize="inherit" />,
+    label: 'Three',
+  },
+  4: {
+    icon: <Looks4Outlined fontSize="inherit" />,
+    label: 'Four',
+  },
+  5: {
+    icon: <Looks5Outlined fontSize="inherit" />,
+    label: 'Five',
+  },
+};
+
+function IconContainer(props: IconContainerProps) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
 export default function Landing() {
-  return <>Home - Coming Soon</>;
+  return (
+    <>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          textAlign: 'center',
+        }}
+      >
+        <div>
+          <Typography>Your Coaching Streak</Typography>
+          <Divider flexItem variant="middle" />
+        </div>
+        <Typography>
+          Talk to your coach every week to unlock your full potential at work.
+        </Typography>
+        <StyledRating
+          IconContainerComponent={IconContainer}
+          size="large"
+          readOnly
+          value={3}
+        />
+      </div>
+    </>
+  );
 }
